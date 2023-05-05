647189e3b554060d56bde0aa796a778a7f3e3645
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-05-05 20:10:46 +0530

    Worked on the schema part

---
+++ src/apollo/schema/user.schema.ts
@@ -0,0 +1,22 @@
+import { Prop } from "@typegoose/typegoose";
+import { Field } from "type-graphql";
+
+export class User {
+    @Field(() => String)
+    _id: string
+    //? Name
+    @Field(() => String)
+    @Prop({ required: true, type: String })
+    name: string
+    //? Username
+    @Field(() => String)
+    @Prop({ required: true, type: String })
+    username: string
+    //? Email
+    @Field(() => String)
+    @Prop({ required: true, type: String })
+    email: string
+    //? Password
+    @Prop({ required: true, type: String })
+    password: string
+}
