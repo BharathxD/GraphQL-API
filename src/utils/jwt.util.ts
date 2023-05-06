603bdd8a2d0feedeb1110ca1e196c2e0ed632010
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-05-06 11:25:05 +0530

    Implement JWT Service class

--- src/utils/jwt.util.ts
+++ src/utils/jwt.util.ts
@@ -3,3 +3,45 @@ import jwt, {
     TokenExpiredError
 } from "jsonwebtoken";
 import { publicKey, privateKey } from "./pemToString.util";
+
+type JwtPayload = string | Buffer | object;
+
+export default class JwtService {
+    private readonly publicKey: string;
+    private readonly privateKey: string;
+    constructor() {
+        try {
+            this.publicKey = Buffer.from(publicKey).toString();
+            this.privateKey = Buffer.from(privateKey).toString();
+        } catch (error: any) {
+            throw new Error(`Failed to load JWT Key files: ${error.message}.\nPlease generate a public and private key pair.`)
+        }
+    }
+    generateToken(payload: JwtPayload): string {
+        try {
+            const expiresIn = process.env.EXPIRES_IN || "60m";
+            const options: jwt.SignOptions = {
+                algorithm: "RS256",
+                expiresIn
+            }
+            return jwt.sign(payload, this.privateKey, options);
+        } catch (error: any) {
+            throw new Error(`Failed to generate jwt token: ${error.message}`);
+        }
+    }
+    verifyToken(token: string): JwtPayload {
+        try {
+            const payload = jwt.verify(token, this.publicKey) as JwtPayload;
+            return payload;
+        } catch (error: any) {
+            if (error instanceof NotBeforeError) {
+                throw NotBeforeError;
+            } else if (error instanceof TokenExpiredError) {
+                throw TokenExpiredError;
+            }
+            else {
+                throw new Error(`Failed to generate JWT Token: ${error.message}`);
+            }
+        }
+    }
+}
