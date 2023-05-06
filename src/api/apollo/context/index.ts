2fbcbc86d8a1e474ab5fc56c5a7899e6519e0686
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-05-06 16:01:33 +0530

    Generate doucmentation for the Context Function in Apollo Server

--- src/api/apollo/context/index.ts
+++ src/api/apollo/context/index.ts
@@ -1,6 +1,12 @@
 import Context from "../../../types/context.types";
 import JwtService from "../../../utils/jwt.util";
 
+/**
+ * This function extracts the access token from the request headers or cookies, verifies it using a JWT service, and adds the user information to the context object.
+ * @param {Context} context - The `context` parameter is an object that contains information about the current request and response, such as the HTTP request and response objects, the URL, and any query parameters. 
+ * In this specific @returns The function `context` returns an updated context object that includes the user information decoded from the access token. If there is no access token, the original context object is returned.
+ */
+
 const context = (context: Context): Context => {
     const { cookies } = context.req;
     const { authorization } = context.req.headers;
