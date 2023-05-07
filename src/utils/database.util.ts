cf1461a072b1f02c35ef4b43f324a8f3bbe015f9
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-05-07 21:19:07 +0530

    Added some documentation to the database connectivity class

--- src/utils/database.util.ts
+++ src/utils/database.util.ts
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 import logger from './logger.util';
 
+/* This is a TypeScript class that connects to a MongoDB Atlas database and allows for disconnection. */
 export default class Database {
   private mongoUrl: string;
 
@@ -12,6 +13,10 @@ export default class Database {
     mongoose.set('strictQuery', false);
   }
 
+  /**
+   * This function connects to a MongoDB Atlas database using a provided URL and logs a success message
+   * or exits the process with an error message if the connection fails.
+   */
   public async connect(): Promise<void> {
     try {
       await mongoose.connect(this.mongoUrl);
@@ -19,6 +24,20 @@ export default class Database {
     } catch (error: any) {
       logger.error(`Error connecting to MongoDB Atlas ❌: ${error.message}`);
       process.exit(1);
+    }
+  }
+  /**
+   * This is a TypeScript function that disconnects from a MongoDB database using Mongoose and returns
+   * a Promise that resolves to void or a string error message.
+   * @returns The `disconnect` method is returning a `Promise` that resolves to `void` if the
+   * disconnection is successful, or a `string` containing the error message if an error occurs during
+   * the disconnection process.
+   */
+  public async disconnect(): Promise<void | string> {
+    try {
+      return await mongoose.disconnect();
+    } catch (error: any) {
+      return error.message;
     }
   }
 }
